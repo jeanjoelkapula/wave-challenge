24e26122c32f3f82fd9b37dabc3bad2361d1057c
Jean Joel Kapula <jeanjoelkapula@gmail.com>
2022-02-17 12:50:33 +0200

    Update models

--- payroll/models.py
+++ payroll/models.py
@@ -3,16 +3,15 @@ from sqlalchemy import null
 
 # Create your models here.
 class JobGroup(models.Model):
-    id = models.CharField(max_length=1, null=False, blank=False)
-    name = models.TextField(max_length=65, null=False, blank=False)
+    id = models.CharField(max_length=1, null=False, blank=False, primary_key=True)
     hourlyRate = models.FloatField(null=False, blank=False)
 
 class Employee(models.Model):
-    employeeId = models.AutoField(primary_key=True)
-    jobGroup = models.ForeignKey(JobGroup, null=False, blank=False, related_name="groupEmployees")
+    employeeId = models.IntegerField(primary_key=True)
+    jobGroup = models.ForeignKey(JobGroup, null=False, blank=False, on_delete=models.DO_NOTHING, related_name="groupEmployees")
 
 class TimeReport(models.Model):
     id = models.IntegerField(null=False, blank=False, primary_key=True)
     date = models.DateField(auto_now_add=True)
     hoursWorked = models.FloatField(null=False)
-    employee = models.ForeignKey(Employee,null=False, blank=False, related_name="timeReports")
+    employee = models.ForeignKey(Employee,null=False, blank=False, on_delete=models.DO_NOTHING, related_name="timeReports")
